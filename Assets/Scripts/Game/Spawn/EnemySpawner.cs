683c4a9587b8b7c81242374eaf4d5058f480c3c1
pauloschade <paulosc4@al.insper.edu.br>
2023-04-22 13:43:50 -0300

    victory condition

--- Assets/Scripts/Game/Spawn/EnemySpawner.cs
+++ Assets/Scripts/Game/Spawn/EnemySpawner.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public List<GameObject> prefabs;
-    public List<Transform> spawnPoints;
-    public float spawnInterval=2f;
+    [SerializeField] private List<GameObject> prefabs;
+    [SerializeField] private List<Transform> spawnPoints;
+    [SerializeField] private float spawnInterval=2f;
+    [SerializeField] private int totalEnemies= 10;
+    [SerializeField] private GameManager gameManager;
+
+    
     public void Start()
     {
+      gameManager = GameManager.GetInstance();
       StartCoroutine(SpawnDelay());
     }
 
     IEnumerator SpawnDelay()
     {
+      if(Check()){
+        gameManager.SetVictory();
+        yield break;
+      } 
       SpawnEnemy();
       yield return new WaitForSeconds(spawnInterval);
       StartCoroutine(SpawnDelay());
@@ -27,6 +36,11 @@ public class EnemySpawner : MonoBehaviour
       enemy.GetComponent<Enemy>().Init();
       float towerSize = enemy.GetComponent<SpriteRenderer>().bounds.size.y/3;
       enemy.transform.position += towerSize * Vector3.up;
+      totalEnemies --;
     }
 
+    private bool Check()
+    {
+      return totalEnemies > 0 ? false : true;
+    }
 }
---
+++ Assets/Scripts/Game/UI/Text/Pannel.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pannel : MonoBehaviour 
+{
+  //deactivate all UI elements
+  [SerializeField] private int _type;
+  private GameManager _gameManager;
+  private bool _canvaSet;
+  private GameObject _canvaUI;
+
+  void Start()
+  {
+    _gameManager = GameManager.GetInstance();
+    _canvaUI = gameObject.transform.GetChild(0).gameObject;
+    _canvaUI.SetActive(false);
+  }
+
+  void Update()
+  {
+    if(_canvaSet) return;
+    if(Check()) {
+      _canvaUI.SetActive(true);
+      _canvaSet = true;
+    }
+  }
+
+  private bool Check()
+  {
+    return _type == 0 ? _gameManager.GameOver : _gameManager.Victory;
+  }
+}
